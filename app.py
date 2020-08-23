80ffe1410481406e6ccb80bebcb8c5dbcb901474
Meredith Murphy <meredithcat@gmail.com>
2020-08-22 18:44:24 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask, request, render_template
+import random
+
+app = Flask(__name__)
+
+def sort_letters(message):
+    """A helper method to sort the characters of a string in alphabetical order
+    and return the new string."""
+    return ''.join(sorted(list(message)))
+
+
+@app.route('/')
+def homepage():
+    """A homepage with handy links for your convenience."""
+    return render_template('home.html')
+
+@app.route('/froyo')
+def choose_froyo():
+    """Shows a form to collect the user's Fro-Yo order."""
+    pass
+
+@app.route('/froyo_results')
+def show_froyo_results():
+    """Shows the user what they ordered from the previous page."""
+    pass
+
+@app.route('/favorites')
+def favorites():
+    """Shows the user a form to choose their favorite color, animal, and city."""
+    pass
+
+@app.route('/favorites_results')
+def favorites_results():
+    """Shows the user a nice message using their form results."""
+    pass
+
+@app.route('/secret_message')
+def secret_message():
+    """Shows the user a form to collect a secret message. Sends the result via
+    the POST method to keep it a secret!"""
+    pass
+
+@app.route('/message_results', methods=['POST'])
+def message_results():
+    """Shows the user their message, with the letters in sorted order."""
+    pass
+
+@app.route('/calculator')
+def calculator():
+    """Shows the user a form to enter 2 numbers and an operation."""
+    return """
+    <form action="/calculator_results" method="GET">
+        Please enter 2 numbers and select an operator.<br/><br/>
+        <input type="number" name="operand1">
+        <select name="operation">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="operand2">
+        <input type="submit" value="Submit!">
+    </form>
+    """
+
+@app.route('/calculator_results')
+def calculator_results():
+    """Shows the user the result of their calculation."""
+    pass
+
+
+# List of compliments to be used in the `compliments_results` route (feel free 
+# to add your own!) 
+# https://systemagicmotives.com/positive-adjectives.htm
+list_of_compliments = [
+    'awesome',
+    'beatific',
+    'blithesome',
+    'conscientious',
+    'coruscant',
+    'erudite',
+    'exquisite',
+    'fabulous',
+    'fantastic',
+    'gorgeous',
+    'indubitable',
+    'ineffable',
+    'magnificent',
+    'outstanding',
+    'propitioius',
+    'remarkable',
+    'spectacular',
+    'splendiferous',
+    'stupendous',
+    'super',
+    'upbeat',
+    'wondrous',
+    'zoetic'
+]
+
+@app.route('/compliments')
+def compliments():
+    """Shows the user a form to get compliments."""
+    return render_template('compliments_form.html')
+
+@app.route('/compliments_results')
+def compliments_results():
+    """Show the user some compliments."""
+    context = {
+        # TODO: Enter your context variables here.
+    }
+
+    return render_template('compliments_results.html', **context)
+
+
+if __name__ == '__main__':
+    app.run()
