9d7ecd4558468bd9065201a04a83348380880b77
Matthew Wei <matthewwei35@gmail.com>
2021-01-28 17:51:33 -0800

    Completed Secret Message

--- app.py
+++ app.py
@@ -61,12 +61,19 @@ def favorites_results():
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
+    return """
+    <form action="/message_results" method="POST">
+        What is your top secret message? <br/>
+        <input type="text" name="message"><br/>
+        <input type="submit" value="Submit Message!">
+    </form>
+    """
 
 @app.route('/message_results', methods=['POST'])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
-    pass
+    users_secret_message = request.form.get('message')
+    return f'{sort_letters(users_secret_message)}'
 
 @app.route('/calculator')
 def calculator():
