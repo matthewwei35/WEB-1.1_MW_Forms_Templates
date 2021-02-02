320a9aff4c79c0ce69b71af147b111cac550e901
Matthew Wei <matthewwei35@gmail.com>
2021-02-01 21:21:53 -0800

    Completed Refactor Fro-Yo

--- app.py
+++ app.py
@@ -17,22 +17,18 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    return """
-    <form action="/froyo_results" method="GET">
-        What is your favorite Fro-Yo flavor? <br/>
-        <input type="text" name="flavor"><br/>
-        What toppings do you like on your Fro-Yo? <br/>
-        <input type="text" name="toppings"><br/>
-        <input type="submit" value="Submit!">
-    </form>
-    """
+    return render_template('froyo_form.html')
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    users_froyo_flavor = request.args.get('flavor')
-    users_froyo_toppings = request.args.get('toppings')
-    return f'You ordered {users_froyo_flavor} flavored Fro-Yo with toppings {users_froyo_toppings}!'
+
+    context = {
+        'users_froyo_flavor' : request.args.get('flavor'),
+        'users_froyo_toppings' : request.args.get('toppings')
+    }
+
+    return render_template('froyo_results.html', **context)
 
 @app.route('/favorites')
 def favorites():
